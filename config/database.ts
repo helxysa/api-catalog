dcd9f668ab3b70ede83d8d1739740796bb2a97fb
diegompap <diego.gomes@mpap.mp.br>
2025-07-04 09:46:05 -0300

    config ssl do node db seed rodar sem exigir ssl do postgres

--- config/database.ts
+++ config/database.ts
@@ -12,7 +12,12 @@ const dbConfig = defineConfig({
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
+        ssl: env.get('DB_SSLMODE') === 'require'
+          ? { rejectUnauthorized: false }
+          : false,
+      
       },
+
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
