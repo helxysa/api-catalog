6adea1474140781df5857e78bcb06ff953bf91f8
Heloysa <heloysagama2@gmail.com>
2025-07-04 09:10:08 -0300

    seeder

--- database/seeders/admin_2_seeder.ts
+++ database/seeders/admin_2_seeder.ts
@@ -1,11 +1,11 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import User from '#models/user'
 
-export default class UserSeeder extends BaseSeeder {
+export default class Admin2Seeder extends BaseSeeder {
   async run() {
     await User.create({
-      fullName: 'Admin User',
-      email: 'admin@admin.mp.br',
+      fullName: 'Admin User 2',
+      email: 'admin2@admin.mp.br',
       password: 'catalog@2025',
       roleId: 2,
     })
